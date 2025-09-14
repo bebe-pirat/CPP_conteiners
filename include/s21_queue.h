82a7e3f99af9231ea13da805b9f5a7dc60babe5a
bebe-pirat <pirat.bebe@gmail.com>
2025-09-14 14:42:19 +0300

    Created folder tests

---
+++ include/s21_queue.h
@@ -0,0 +1,49 @@
+#include <exception>
+#include <stdexcept>
+
+template<typename Type>
+class S21Queue {
+private: 
+    struct Node {
+        Type data;
+        Node* next;
+        Node(const Type& value) : data(value), next(nullptr) {}
+    };
+    
+    Node* head_;
+    Node* tail_;
+    size_t size_;
+    
+public: 
+    // Конструкторы
+    S21Queue();
+    S21Queue(const S21Queue& other); // Конструктор копирования
+    S21Queue(S21Queue&& other); // Конструктор перемещения
+    
+    // Деструктор
+    ~S21Queue();
+    
+    // Операторы
+    S21Queue& operator=(const S21Queue& other); // Копирующее присваивание
+
+    // Функция проверяющая пустая очередь или нет
+    bool empty() const;
+    
+    // Функция возвращающая размер очереди
+    size_t size() const;
+    
+    // Добавление элемента в конец
+    void push(const Type& value);
+    
+    // Удаление элемента сначала
+    void pop();
+    
+    // Получение первого элемента
+    Type& front();
+    
+    // Получение последнего элемента
+    Type& back();
+    
+    // Обмен содержимым
+    void swap(S21Queue& other);
+};
