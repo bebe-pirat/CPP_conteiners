024264f2c453efd8b8480bacd2b02a7d46c30319
bebe-pirat <pirat.bebe@gmail.com>
2025-09-14 13:47:57 +0300

    Update s21_queue.h, new declaration of class

--- src/s21_queue.h
+++ src/s21_queue.h
@@ -2,24 +2,50 @@
 #include <stdexcept>
 
 template<typename Type>
-
 class S21Queue {
-    private: 
-        int capacity_;
-        int lenght_;
-        Type& front_;
-        Type& back_;
-    public: 
-        // функция проверяющая пустая очередь или нет
-        bool empty();
-        // функция возвращающая размер очереди
-        int size();
-        // добавление элемента в конец
-        void push();
-        // удаление элемента сначала
-        void pop();
-        // получение первого элемента
-        Type front();
-        // получение последнего элемента
-        Type back();
+private: 
+    struct Node {
+        Type data;
+        Node* next;
+        Node(const Type& value) : data(value), next(nullptr) {}
+    };
+    
+    Node* head_;
+    Node* tail_;
+    size_t size_;
+    
+public: 
+    // Конструкторы
+    S21Queue();
+    S21Queue(const S21Queue& other); // Конструктор копирования
+    S21Queue(S21Queue&& other); // Конструктор перемещения
+    
+    // Деструктор
+    ~S21Queue();
+    
+    // Операторы
+    S21Queue& operator=(const S21Queue& other); // Копирующее присваивание
+    
+    // Функция проверяющая пустая очередь или нет
+    bool empty() const;
+    
+    // Функция возвращающая размер очереди
+    size_t size() const;
+    
+    // Добавление элемента в конец
+    void push(const Type& value);
+    
+    // Удаление элемента сначала
+    void pop();
+    
+    // Получение первого элемента
+    Type& front();
+    const Type& front() const;
+    
+    // Получение последнего элемента
+    Type& back();
+    const Type& back() const;
+    
+    // Обмен содержимым
+    void swap(S21Queue& other) noexcept;
 };
