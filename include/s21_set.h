83bee8dfbf4697e732a85d389e8c43f9abaf06cc
bebe-pirat <pirat.bebe@gmail.com>
2025-09-16 14:16:51 +0300

    UPdate s21_set.h, constructors are added

--- include/s21_set.h
+++ include/s21_set.h
@@ -9,8 +9,8 @@ class S21Set {
         uint32_t size_ = 100;
     public: 
     S21Set();
-    S21Set(const S21Queue& other); // Конструктор копирования
-    S21Set(S21Queue&& other); // Конструктор перемещения
+    S21Set(const S21Set& other); // Конструктор копирования
+    S21Set(S21Set&& other); // Конструктор перемещения
 
     ~S21Set();
 
@@ -19,3 +19,24 @@ class S21Set {
     bool contains(const Type& value);
     binary_search();
 };
+
+template<typename Type> 
+S21Set<Type>::S21Set() {
+    array_  = new Type[capacity_];
+}
+
+template<typename Type>
+S21Set<Type>::S21Set(const S21Set& other): array_(other.array_), capacity_(other.capacity_), size_(other.size_);
+
+template<typename Type>
+S21Set<Type>::S21Set(const S21Set& other): array_(other.array_), capacity_(other.capacity_), size_(other.size_) {
+    other.array_ = nullptr;
+    other.capacity_ = 0;
+    other.size_ = 0;
+};
+
+template<typename Type> 
+S21Set<Type>::~S21Set() 
+{ 
+    delete[] array_;
+}
